a647aca95400c4f92aa8283c6085650f4bf3ad9e
Emily Tracey <emilytracey21@gmail.com>
2023-04-06 16:26:04 +0100

    RoboFriends app with React

---
+++ src/containers/App.js
@@ -0,0 +1,62 @@
+import React, {Component} from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+// import { robots } from "./robots";
+import Scroll from "../components/Scroll";
+import './app.css';
+
+// const state = {
+//     robots: robots,
+//     searchfield: ''
+// }
+
+class App  extends Component{
+    constructor() {
+        super()
+        // describe app and can change and effect the app
+        // app owns the state
+        // virtual DOM collects the state and React uses to render
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+        // console.log('1');
+    }
+
+    // make requests
+    componentDidMount() {
+        // fetch allows you to make requests on servers
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => {this.setState({robots: users})});
+        // console.log('2');
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+    }
+
+    render() {
+        const {robots, searchfield} = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+        })
+        // ternary operator
+        return !robots.length ?
+            <h1>Loading</h1> :
+        (
+        // console.log('3');
+            <div className="tc">
+                <h1 className="f1">RoboFriends</h1>
+                {/* runs function when event occurs and updates the state */}
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                    <CardList robots={filteredRobots}/>
+                </Scroll>
+            </div>
+        );
+    }    
+}
+
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './containers/App';
+import 'tachyons';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
