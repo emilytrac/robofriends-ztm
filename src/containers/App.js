244bb99fbb0dbf5ba56d1f792f2b6ea3cf10ec7d
Emily Tracey <emilytracey21@gmail.com>
2023-04-18 10:55:22 +0100

    Changing to use React Hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 // import { robots } from "./robots";
@@ -6,60 +6,69 @@ import Scroll from "../components/Scroll";
 import ErrorBoundary from "../components/ErrorBoundary";
 import './app.css';
 
-// const state = {
-//     robots: robots,
-//     searchfield: ''
-// }
+function App() {
+    // constructor() {
+    //     super()
+    //     // describe app and can change and effect the app
+    //     // app owns the state
+    //     // virtual DOM collects the state and React uses to render
+    //     this.state = {
+    //         robots: [],
+    //         searchfield: ''
+    //     }
+    //     // console.log('1');
+    // }
 
-class App  extends Component{
-    constructor() {
-        super()
-        // describe app and can change and effect the app
-        // app owns the state
-        // virtual DOM collects the state and React uses to render
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-        // console.log('1');
-    }
+    // set to initial state
+    const [robots, setRobots] = useState([])
+    const [searchfield, setSearchField] = useState('')
+    const [count, setCount] = useState(0)
 
-    // make requests
-    componentDidMount() {
-        // fetch allows you to make requests on servers
+    // componentDidMount() {
+    //     // fetch allows you to make requests on servers
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //     .then(response => response.json())
+    //     .then(users => {this.setState({robots: users})});
+    //     // console.log('2');
+    // }
+
+    // by default runs after rendering
+    // componentDidMount shortcut with []
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
-        .then(users => {this.setState({robots: users})});
-        // console.log('2');
+        .then(users => {setRobots(users)});
+        console.log(count)
+    },[count]) // only run if count changes
+
+    const onSearchChange = (event) => {
+        // this.setState({searchfield: event.target.value})
+        setSearchField(event.target.value)
     }
 
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value})
-    }
-
-    render() {
-        const {robots, searchfield} = this.state;
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-        })
-        // ternary operator
-        return !robots.length ?
-            <h1>Loading</h1> :
-        (
-        // console.log('3');
-            <div className="tc">
-                <h1 className="f1">RoboFriends</h1>
-                {/* runs function when event occurs and updates the state */}
-                <SearchBox searchChange={this.onSearchChange}/>
-                <Scroll>
-                    <ErrorBoundary>
-                        <CardList robots={filteredRobots}/>
-                    </ErrorBoundary>
-                </Scroll>
-            </div>
-        );
-    }    
+    // const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
+    // ternary operator
+    return !robots.length ?
+        <h1>Loading</h1> :
+    (
+    // console.log('3');
+        <div className="tc">
+            <h1 className="f1">RoboFriends</h1>
+            <button onClick={()=>setCount(count+1)}>Click me</button>
+            {/* runs function when event occurs and updates the state */}
+            {/* no longer in a class - don't need this keyword */}
+            <SearchBox searchChange={onSearchChange}/>
+            <Scroll>
+                <ErrorBoundary>
+                    <CardList robots={filteredRobots}/>
+                </ErrorBoundary>
+            </Scroll>
+        </div>
+    );
+        
 }
-
 
 export default App;
